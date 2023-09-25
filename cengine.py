20be8d758cbdf4ee5907ff599b013a4501fb1d2c
Naman Sharma <namansharma18899@gmail.com>
2023-09-25 22:53:57 +0530

    Added banner, better ui

--- cengine.py
+++ cengine.py
@@ -1,4 +1,5 @@
 import subprocess
+from utils.utility import create_cefd_banner, get_compression_ratio, print_colored
 import argparse
 import os
 from compression.bwt import BWT
@@ -21,7 +22,6 @@ decoded_file_extension = ".dec"
 def encode_string_buffer(string_buffer):
     # bwt = BWT()
     # bwt = bwt.encode(string_buffer)
-    logger.info("Encoding String")
     rle_obj = RLE()
     compress_char_arr = rle_obj.encode(string_buffer)
     return compress_char_arr
@@ -29,12 +29,11 @@ def encode_string_buffer(string_buffer):
 
 def decode_string_buffer(string_buffer):
     rle_obj = RLE()
-    logger.info("Decoding String")
     compress_char_arr = rle_obj.decode(string_buffer)
     # bwt = BWT()
     # bwt = bwt.decode(compress_char_arr)
     # bwt = compress_char_arr[::-1][1:]
-    return compress_char_arr 
+    return compress_char_arr
 
 
 def write_file(file, buffer):
@@ -45,38 +44,65 @@ def write_file(file, buffer):
     outF.write(buffer)
 
 
-def handle_file(file):
+def get_resultant_file(original_file, transform):
+    res_file_extension = encoded_file_extension if transform else decoded_file_extension
+    y = os.path.basename(original_file)
+    y = y.split(".")
+    return (
+        original_file.split(os.path.basename(original_file))[0]
+        + y[0]
+        + res_file_extension
+        + "."
+        + y[1]
+    )
+
+
+def handle_file(file: str, transform: bool):
     f = open(file, "rb")
+    result_file_loc = get_resultant_file(file, transform)
     while True:
-        str_buffer = f.read(1024).decode(encoding="utf-8")
+        str_buffer = f.read().decode(
+            encoding="utf-8"
+        )  # for good performance batch it, ex: read(1024)
         if not str_buffer:
             break
-        if encode:
-            resultant_file = encode_string_buffer(str_buffer)
+        if transform:
+            result_buffer = encode_string_buffer(str_buffer)
         else:  # decode
-            resultant_file = decode_string_buffer(str_buffer)
-        res_file_extension = (
-            encoded_file_extension if encode else decoded_file_extension
-        )
-        y = os.path.basename(file)
-        y = y.split(".")
-        newF = (
-            file.split(os.path.basename(file))[0]
-            + y[0]
-            + res_file_extension
-            + "."
-            + y[1]
-        )
-        write_file(newF, resultant_file)
+            result_buffer = decode_string_buffer(str_buffer)
+        write_file(result_file_loc, result_buffer)
     f.close()
 
 
+class Colors:
+    red = "red"
+    green = "green"
+    yellow = "yellow"
+    blue = "blue"
+    purple = "purple"
+    cyan = "cyan"
+
+
 if __name__ == "__main__":
+    print(create_cefd_banner())
     enc_input_file = "/home/namansh/personal/projects/cefd/assets/temp.text"
     dec_input_file = "/home/namansh/personal/projects/cefd/assets/temp.enc.text"
     dec_output_file = "/home/namansh/personal/projects/cefd/assets/temp.dec.enc"
-    encode= True 
-    handle_file(file=enc_input_file)
-    encode=False
-    handle_file(file=dec_input_file)
-    print(subprocess.run(f'md5sum {enc_input_file} {dec_output_file}', shell=True, check=True))
+    print("Compressing data:")
+    clrs = Colors()
+    print(
+        f"Original Size: {print_colored(text=os.path.getsize(enc_input_file), color=clrs.blue)} bytes"
+    )
+    handle_file(file=enc_input_file, transform=True)
+    handle_file(file=dec_input_file, transform=False)
+    print(
+        f"Compressed Size: {print_colored(text=os.path.getsize(dec_input_file),  color=clrs.yellow)} bytes"
+    )
+    print(
+        f"Compressed Ratio: {print_colored(text=get_compression_ratio(enc_input_file, dec_input_file ),  color=clrs.green)}"
+    )
+    logger.info(
+        subprocess.run(
+            f"md5sum {enc_input_file} {dec_output_file}", shell=True, check=True
+        )
+    )
