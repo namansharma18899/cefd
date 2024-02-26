7dd354d637333f5a9f24e404af8e133fc184073a
Naman Sharma <namansharma18899@gmail.com>
2024-02-26 23:26:42 +0530

    Added huffman Encoding, pending write bytes to flie with metadata

--- compression/huffman_encoding.py
+++ compression/huffman_encoding.py
@@ -1,85 +1,93 @@
-# A Huffman Tree Node
-import heapq
-from compression.cmp_interface import GenericCompressionInterface
+from collections import Counter
+import math
+from copy import deepcopy
 
 
-class node:
-    def __init__(self, freq, symbol, left=None, right=None):
-        # frequency of symbol
-        self.freq = freq
-
-        # symbol name (character)
-        self.symbol = symbol
-
-        # node left of current node
-        self.left = left
-
-        # node right of current node
-        self.right = right
-
-        # tree direction (0/1)
-        self.huff = ""
-
-    def __lt__(self, nxt):
-        return self.freq < nxt.freq
-
-
-# utility function to print huffman
-# codes for all symbols in the newly
-# created Huffman tree
-def printNodes(node, val=""):
-    # huffman code for current node
-    newVal = val + str(node.huff)
-
-    # if node is not an edge node
-    # then traverse inside it
-    if node.left:
-        printNodes(node.left, newVal)
-    if node.right:
-        printNodes(node.right, newVal)
-
-        # if node is edge node then
-        # display its huffman code
-    if not node.left and not node.right:
-        print(f"{node.symbol} -> {newVal}")
-
-# converting characters and frequencies
-# into huffman tree nodes
-class HuffmanEncoding(GenericCompressionInterface):
+class HuffmanEncoding():
     def __init__(self) -> None:
-        super().__init__()
-
-    def encode(self, frequency_map: dict, *args, **kwargs):
-        nodes = []
-        for char, fre in frequency_map.items():
-            heapq.heappush(nodes, node(fre, char))
-        while len(nodes) > 1:
-            # sort all the nodes in ascending order
-            # based on their frequency
-            left = heapq.heappop(nodes)
-            right = heapq.heappop(nodes)
-            # assign directional value to these nodes
-            left.huff = 0
-            right.huff = 1
-            # combine the 2 smallest nodes to create
-            # new node as their parent
-            newNode = node(left.freq + right.freq, left.symbol + right.symbol, left, right)
-            heapq.heappush(nodes, newNode)
-        return nodes
-
-    def decode(self, input_str, *args, **kwargs):
         pass
 
-# Huffman Tree is ready!
-# printNodes(nodes[0])
-# obj = HuffmanEncoding()
-# nodes = obj.encode({
-#     "a":5,
-#     "b":9,
-#     "c":12,
-#     "d":13,
-#     "e":16,
-#     "f":45,
-#     })
+    def encode(self, input_str: str):
+        self.huffman_string = deepcopy(input_str)
+        freq_table = dict(Counter(input_str))
+        max_2_pow =1
+        cn  = 0
+        while(max_2_pow < len(freq_table)):
+            max_2_pow = max_2_pow<<1
+            cn+=1 
+        next_bin = '0'*cn 
+        nums = set(freq_table.keys())
+        self.freq_map = {}
+        for k,v in freq_table.items():
+            self.freq_map[k] = next_bin
+            self.huffman_string.replace(k, next_bin, v)
+            next_bin = bin(int(f'0b{next_bin}',2)+1).split('0b')[1].zfill(cn)
 
-# printNodes(nodes[0])
+    
+# obj = HuffmanEncoding()
+# obj.encode('AAZZIIabbcccdddeeeffaafaedd')
+# print(obj.huffman_string)
+
+
+
+# Huffman Coding in python
+
+string = 'BCAADDDCCACACAC'
+
+
+# Creating tree nodes
+class NodeTree(object):
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return (self.left, self.right)
+
+    def nodes(self):
+        return (self.left, self.right)
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+
+# Main function implementing huffman coding
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
+
+
+# Calculating frequency
+freq: dict = {}
+for c in string:
+    if c in freq:
+        freq[c] += 1
+    else:
+        freq[c] = 1
+
+freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+nodes = freq
+
+while len(nodes) > 1:
+    (key1, c1) = nodes[-1]
+    (key2, c2) = nodes[-2]
+    nodes = nodes[:-2]
+    node = NodeTree(key1, key2)
+    nodes.append((node, c1 + c2))
+    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+print(nodes[0][0])
+huffmanCode = huffman_code_tree(nodes[0][0])
+
+
+print(' Char | Huffman code ')
+print('----------------------')
+for (char, frequency) in freq:
+    print(' %-4r |%12s |%12s' % (char, huffmanCode[char], frequency))
